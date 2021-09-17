d10e0c5636ec037d3869c23b3e64a4e125ec84c3
Peter Kofler <peter.kofler@code-cop.org>
2021-09-17 11:31:16 +0200

    failing assertTrue should set state to failed.

--- src/amazing/AmazingUnitTest.java
+++ src/amazing/AmazingUnitTest.java
@@ -2,7 +2,29 @@ package amazing;
 
 public class AmazingUnitTest {
 
+    private static boolean success = true;
+
     public static void main(String[] args) {
-        System.out.println("Green");
+        // failing assertTrue should set state to failed
+        assertTrue(false);
+        // assert: will sehen Red
+        if (success) {
+            System.out.println("Green");
+        } else {
+            System.out.println("Red");
+        }
+
+    }
+
+    private static void assertTrue(boolean b) {
+        success = false;
     }
 }
+
+/*
+ * Test List
+ * - assertTrue
+ * - assertEqual
+ * - define test, find test, run test
+ * - fixtures (before, after)
+ */

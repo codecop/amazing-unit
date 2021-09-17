6063c24c2da0338a5d58e4f89e0bf6331a6cacde
Peter Kofler <peter.kofler@code-cop.org>
2021-09-17 11:37:16 +0200

    (3) failed followed by success should leave state to false.

--- src/amazing/AmazingUnitTest.java
+++ src/amazing/AmazingUnitTest.java
@@ -6,26 +6,40 @@ public class AmazingUnitTest {
 
     public static void main(String[] args) {
         // (1) failing assertTrue should set state to failed
+        success = true;
         assertTrue(false);
-        // assert: will sehen Red
         if (success) {
             System.out.println("Green");
         } else {
+            // assert: will sehen Red
             System.out.println("expected: Red");
         }
 
         // (2) success assertTrue should set state to true
+        success = true;
         assertTrue(true);
-        // assert: will Green
         if (success) {
+            // assert: will Green
             System.out.println("expected: Green");
         } else {
             System.out.println("Red");
         }
+
+        // (3) failed followed by success should leave state to false
+        success = true;
+        assertTrue(false);
+        assertTrue(true);
+        if (success) {
+            System.out.println("Green");
+        } else {
+            // assert: will sehen Red
+            System.out.println("expected: Red");
+        }
+        // TODO refactor duplication
     }
 
     private static void assertTrue(boolean b) {
-        success = b;
+        success = success && b;
     }
 }
 

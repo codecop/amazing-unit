68cd509722188c9291e24e9e287738b559ebc1de
Peter Kofler <peter.kofler@code-cop.org>
2021-09-17 11:34:34 +0200

    (2) success assertTrue should set state to true

--- src/amazing/AmazingUnitTest.java
+++ src/amazing/AmazingUnitTest.java
@@ -5,19 +5,27 @@ public class AmazingUnitTest {
     private static boolean success = true;
 
     public static void main(String[] args) {
-        // failing assertTrue should set state to failed
+        // (1) failing assertTrue should set state to failed
         assertTrue(false);
         // assert: will sehen Red
         if (success) {
             System.out.println("Green");
         } else {
-            System.out.println("Red");
+            System.out.println("expected: Red");
         }
 
+        // (2) success assertTrue should set state to true
+        assertTrue(true);
+        // assert: will Green
+        if (success) {
+            System.out.println("expected: Green");
+        } else {
+            System.out.println("Red");
+        }
     }
 
     private static void assertTrue(boolean b) {
-        success = false;
+        success = b;
     }
 }
 
